d7dd59649a03e73fc4e58018950b69f6f5d5dd08
Christian Toro <torochristian97@gmail.com>
2020-01-22 21:52:51 -0500

    First Commit

---
+++ android/app/src/main/kotlin/com/example/uf_ride_share_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.uf_ride_share_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
