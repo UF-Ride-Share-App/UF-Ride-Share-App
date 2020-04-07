6b71c57ac1336916f3ae9447c4754b5dd5e2ec9b
maxbarkow <maxbarkow@ufl.edu>
2020-04-06 22:24:28 -0400

    ride card changes

--- lib/components/ride_card_prompt.dart
+++ lib/components/ride_card_prompt.dart
@@ -9,16 +9,36 @@ class RideCartPrompt extends StatelessWidget {
         return AlertDialog(
           title: Text('Ride Details', textAlign: TextAlign.center,),
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-          elevation: 120,
-          child: Container(
-            height: 20.0,
-            width: 20.0,
-            color: Colors.orange,          
+
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Gainesville, FL',style: TextStyle(height: 1)),
+              Text('to', textAlign: TextAlign.center,style: TextStyle(height: 1.5)),
+              Text('Miami, FL' , style: TextStyle(height: 1.5),),
+              Row(children: <Widget>[
+              Text('June 13th, 2020',style: TextStyle(height: 1.5)), Text('|',style: TextStyle(height: 1.5)), Text(' 3:00PM',style: TextStyle(height: 1.5))],),
+              Row(children: <Widget>[
+                Text('Seats Available: ',style: TextStyle(height: 1.5)),
+                Text('3' ,style: TextStyle(height: 1.5))
+              ],),
+              Text(' ',style: TextStyle(height: 1.5)),
+              Text('Description: This is where the driver can add a description regarding anything from luggage to pets' ,style: TextStyle(height: 1.5))
+              
+              
+            ],
+                
           ),
           actions: <Widget>[
             MaterialButton(
-              child: Text('Join Ride ', textAlign: TextAlign.left,),
-              onPressed: () {print('ride join clicked');}
+              child: Text('Join Ride ', textAlign: TextAlign.center,),
+              color: Colors.tealAccent[700] ,
+              shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              onPressed: () {
+                 Navigator.of(context).pop();
+                print('ride join clicked');}
             ),
           ],
         );
