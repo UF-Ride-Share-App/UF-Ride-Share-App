4b05c1884f225ba454dbc75ca480401b86debb0f
aisaly <alexandraisaly@gmail.com>
2020-04-08 17:34:09 -0400

    remove unused import

--- lib/ui/posting/posting_list.dart
+++ lib/ui/posting/posting_list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uf_ride_share_app/models/ride.dart';
 
 class PostList extends StatefulWidget {
