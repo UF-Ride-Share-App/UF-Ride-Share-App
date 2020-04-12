38d96de6f0a9b42764263df4cf0d8ded3ac3ab7e
aisaly <alexandraisaly@gmail.com>
2020-04-11 21:03:07 -0400

    set-up

--- lib/ui/landing/landing.dart
+++ lib/ui/landing/landing.dart
@@ -1,44 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:uf_ride_share_app/ui/posting/posting_list.dart';
 import 'package:uf_ride_share_app/ui/landing/search.dart';
 
-class Landing extends StatelessWidget {
-  Landing();
+Stream<QuerySnapshot> stream;
 
+class Landing extends StatefulWidget {
+
+  @override
+  _LandingState createState() => _LandingState();
+}
+
+class _LandingState extends State<Landing> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightGreenAccent,
-        title: Text("Share & Ride"),
-        centerTitle: true,
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 20.0),
-            child: GestureDetector(
-              onTap: () {},
-              child: Icon(Icons.filter_list),
+        appBar: AppBar(
+          backgroundColor: Colors.lightGreenAccent,
+          title: Text("Share & Ride"),
+          centerTitle: true,
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Icon(Icons.filter_list),
+              ),
             ),
-          ),
-        ],
-        actionsIconTheme: IconThemeData(
-          color: Colors.tealAccent[700],
-          size: 30.0,
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            Container(height: 250, child: Search()),
-            Divider(color: Colors.grey),
-            Expanded(
-              flex: 11,
-              child: PostList(),
-            )
           ],
-        )
-      )
-    );
+          actionsIconTheme: IconThemeData(
+            color: Colors.tealAccent[700],
+            size: 30.0,
+          ),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: <Widget>[
+                Container(height: 250, child: Search()),
+                Divider(color: Colors.grey),
+                Expanded(
+                  flex: 11,
+                  child: PostList(stream),
+                )
+              ],
+            )));
   }
 }
