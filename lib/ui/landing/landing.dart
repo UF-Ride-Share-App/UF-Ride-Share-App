caa068851a9fe1ac2d1629c067ba671478ff7433
aisaly <alexandraisaly@gmail.com>
2020-04-13 21:14:17 -0400

    Added filtering of landing page results based on search

--- lib/ui/landing/landing.dart
+++ lib/ui/landing/landing.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:uf_ride_share_app/ui/posting/posting_list.dart';
+import 'package:uf_ride_share_app/models/ride.dart';
+import 'package:uf_ride_share_app/components/ride_card.dart';
 import 'package:uf_ride_share_app/ui/landing/search.dart';
 
 class Landing extends StatefulWidget {
@@ -13,6 +14,8 @@ class _LandingState extends State<Landing> {
       Firestore.instance.collection('Rides').snapshots();
   String startLocation = '';
   String endLocation = '';
+  DateTime upperTime;
+  DateTime lowerTime;
 
   @override
   Widget build(BuildContext context) {
@@ -44,30 +47,59 @@ class _LandingState extends State<Landing> {
                     child: Search(
                         onSearch: (String start, String end, DateTime time) {
                       setState(() {
+                        startLocation = start;
+                        endLocation = end;
                         if (time != null) {
-                          Timestamp lowerRange = Timestamp.fromDate(
-                              DateTime.utc(time.year, time.month, time.day));
-                          Timestamp upperRange = Timestamp.fromDate(DateTime.utc(
-                              time.year, time.month, time.day, 23, 59, 59));
-                          stream = Firestore.instance
-                              .collection('Rides')
-                              .where('time', isGreaterThanOrEqualTo: lowerRange)
-                              .where('time', isLessThanOrEqualTo: upperRange)
-                              .snapshots();
-                        } else {
-                          print('no time');
-                          stream = Firestore.instance
-                              .collection('Rides')
-                              .snapshots();
+                          lowerTime = DateTime(time.year, time.month, time.day);
+                          upperTime = DateTime(
+                              time.year, time.month, time.day, 23, 59, 59);
                         }
                       });
                     })),
                 Divider(color: Colors.grey),
                 Expanded(
                   flex: 11,
-                  child: PostList(stream),
-                )
+                  child: _buildStream(context),
+                ),
               ],
             )));
+  }
+
+  Widget _buildStream(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: lowerTime != null
+          ? Firestore.instance
+            .collection('Rides')
+            .where('time', isGreaterThanOrEqualTo: Timestamp.fromDate(lowerTime))
+            .where('time', isLessThanOrEqualTo: Timestamp.fromDate(upperTime))
+            .snapshots()
+          : Firestore.instance.collection('Rides')
+            .where('time', isGreaterThanOrEqualTo: Timestamp.fromDate(DateTime.now()))
+            .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        return _buildList(
+            context, filterSnapshotResults(snapshot.data.documents));
+      },
+    );
+  }
+
+  Widget _buildList(BuildContext context, List<Ride> rides) {
+    return ListView(
+        padding: const EdgeInsets.only(top: 20.0),
+        children: rides.map((ride) => _buildListItem(context, ride)).toList());
+  }
+
+  Widget _buildListItem(BuildContext context, Ride ride) {
+    return RideCard(ride: ride);
+  }
+
+  List<Ride> filterSnapshotResults(List<DocumentSnapshot> documents) {
+    List<Ride> results = documents.map((data) => Ride.fromSnapshot(data)).where(
+        (ride) =>
+            (ride.startLocation == startLocation || startLocation == '') &&
+            (ride.endLocation == endLocation || endLocation == '')).toList();
+    results.sort((rideA, rideB) => rideA.time.compareTo(rideB.time));
+    return results;
   }
 }
