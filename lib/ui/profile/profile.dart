d0e72f76ac4d639f2215f644e2cafb418de50643
aisaly <alexandraisaly@gmail.com>
2020-04-15 14:32:09 -0400

    user name and photourl used

--- lib/ui/profile/profile.dart
+++ lib/ui/profile/profile.dart
@@ -4,84 +4,104 @@ import 'package:uf_ride_share_app/ui/profile/history.dart';
 import 'package:uf_ride_share_app/ui/profile/postings.dart';
 import 'package:uf_ride_share_app/ui/profile/upcoming.dart';
 import 'package:uf_ride_share_app/utils/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'profile_header.dart';
 import '../../styles/style.dart';
 
-class Profile extends StatefulWidget { 
+class Profile extends StatefulWidget {
+  final _auth = new AuthProvider();
+
   @override
   _ProfileMainHeader createState() => _ProfileMainHeader();
 }
 
 class _ProfileMainHeader extends State<Profile> {
- 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DefaultTabController(
-        length: 4,
-        child: NestedScrollView(
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                expandedHeight: MediaQuery.of(context).size.height * 0.40,
-                floating: false,
-                pinned: true,
-                backgroundColor: Colors.lightGreenAccent,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: ProfileHeader(),
-                  centerTitle: true,
-                  title: Text('John Doe', style: FlexibleSpaceBarTextStyle)
-                ),
-                actions: <Widget>[
-                  PopupMenuButton(
-                    onSelected: settingsButtonAction,
-                    icon: Icon(Icons.settings), 
-                    itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                      const PopupMenuItem<String>(
-                        value: 'Logout',
-                        child: ListTile(
-                          leading: Icon(Icons.exit_to_app),
-                          title: Text('Logout'),
-                        ),
+        body: DefaultTabController(
+            length: 4,
+            child: NestedScrollView(
+              headerSliverBuilder:
+                  (BuildContext context, bool innerBoxIsScrolled) {
+                return <Widget>[
+                  SliverAppBar(
+                    expandedHeight: MediaQuery.of(context).size.height * 0.40,
+                    floating: false,
+                    pinned: true,
+                    backgroundColor: Colors.lightGreenAccent,
+                    flexibleSpace: FutureBuilder(
+                      future: this.widget._auth.getCurrentFirebaseUser(),
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        return FlexibleSpaceBar(
+                            background: Center(
+                              child: Container(
+                                width: 60.0,
+                                height: 60.0,
+                                decoration: new BoxDecoration(
+                                  image: new DecorationImage (
+                                    image: NetworkImage(snapshot.data.photoUrl),
+                                    fit: BoxFit.cover,
+                                  ),
+                                  border: Border.all(
+                                    color: Colors.teal[300],
+                                    width: 5,
+                                  ),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                              ),
+                            ),
+                            centerTitle: true,
+                            title: Text(snapshot.data.displayName,
+                                style: FlexibleSpaceBarTextStyle));
+                      },
+                    ),
+                    actions: <Widget>[
+                      PopupMenuButton(
+                        onSelected: settingsButtonAction,
+                        icon: Icon(Icons.settings),
+                        itemBuilder: (BuildContext context) =>
+                            <PopupMenuEntry<String>>[
+                          const PopupMenuItem<String>(
+                            value: 'Logout',
+                            child: ListTile(
+                              leading: Icon(Icons.exit_to_app),
+                              title: Text('Logout'),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
+                    actionsIconTheme:
+                        IconThemeData(size: 30.0, color: Colors.teal[700]),
                   ),
-                ],
-                actionsIconTheme: IconThemeData(
-                  size: 30.0,
-                  color: Colors.teal[700]
-                ),
-              ),
-              SliverPersistentHeader(
-                delegate: _SliverAppBarTabBar(
-                  TabBar(
-                    // onTap: onTabTapped,
-                    labelColor: Colors.black,
-                    unselectedLabelColor: Colors.black,
-                    indicatorColor: Colors.lightGreenAccent,
-                    isScrollable: true,    
-                    tabs: [
-                      Tab(text: 'Postings'),
-                      Tab(text: 'Future Trips'),
-                      Tab(text: 'Ride History'),
-                      Tab(text: 'Drive History'),
-
-                    ]),
-                ),
-                pinned: true,
-              ),
-              // SliverFillRemaining(),
-            ];
-          },
-          body: TabBarView(children: [
-            Postings(),
-            Upcoming(),
-            History(),
-            DriveHistory(),
-          ]),
-        )
-      )
-    );
+                  SliverPersistentHeader(
+                    delegate: _SliverAppBarTabBar(
+                      TabBar(
+                          // onTap: onTabTapped,
+                          labelColor: Colors.black,
+                          unselectedLabelColor: Colors.black,
+                          indicatorColor: Colors.lightGreenAccent,
+                          isScrollable: true,
+                          tabs: [
+                            Tab(text: 'My Posts'),
+                            Tab(text: 'Future Trips'),
+                            Tab(text: 'Past Rides'),
+                            Tab(text: 'Past Drives'),
+                          ]),
+                    ),
+                    pinned: true,
+                  ),
+                  // SliverFillRemaining(),
+                ];
+              },
+              body: TabBarView(children: [
+                Postings(),
+                Upcoming(),
+                History(),
+                DriveHistory(),
+              ]),
+            )));
   }
 
   // void onTabTapped(int index) {
@@ -89,8 +109,7 @@ class _ProfileMainHeader extends State<Profile> {
   // }
 
   void settingsButtonAction(String choice) {
-    if(choice == 'Logout')
-    {
+    if (choice == 'Logout') {
       AuthProvider().logOut();
     }
   }
@@ -109,13 +128,12 @@ class _SliverAppBarTabBar extends SliverPersistentHeaderDelegate {
 
   @override
   Widget build(
-    BuildContext context, double shrinkOffset, bool overlapsContent) {
-      return new Container(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new Container(
         child: new Material(
-          color: Colors.teal[300],
-          child: _tabBar,
-      )
-    );
+      color: Colors.teal[300],
+      child: _tabBar,
+    ));
   }
 
   @override
